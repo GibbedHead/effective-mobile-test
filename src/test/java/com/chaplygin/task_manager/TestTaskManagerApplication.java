bb466ab141dba41887119fcc8fe15ffcba2d0fdc
GibbedHead <chaplygin-ma@yandex.ru>
2024-08-15 17:35:57 +0300

    Импорт проекта из start.spring.io

---
+++ src/test/java/com/chaplygin/task_manager/TestTaskManagerApplication.java
@@ -0,0 +1,11 @@
+package com.chaplygin.task_manager;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TaskManagerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
