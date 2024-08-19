04077771a7569b6765d730e2263ba04df4351806
GibbedHead <chaplygin-ma@yandex.ru>
2024-08-19 21:22:40 +0300

    Тесты ограничений dto запроса на вход

--- src/test/java/com/chaplygin/task_manager/testDataFactory/SignInRequestFactory.java
+++ src/test/java/com/chaplygin/task_manager/testDataFactory/SignInRequestFactory.java
@@ -11,4 +11,53 @@ public class SignInRequestFactory {
                 "1"
         );
     }
+
+    public static SignInRequest createSignInRequestNullEmail() {
+
+        return new SignInRequest(
+                null,
+                "1"
+        );
+    }
+
+    public static SignInRequest createSignInRequestNullPassword() {
+
+        return new SignInRequest(
+                "user1@domain.com",
+                null
+        );
+    }
+
+    public static SignInRequest createSignInRequestEmailShorterThan6Characters() {
+
+        return new SignInRequest(
+                "u@d.c",
+                "1"
+        );
+    }
+
+    public static SignInRequest createSignInRequestEmailLongerThan50Characters() {
+
+        return new SignInRequest(
+                "a".repeat(50) + "@domain.com",
+                "1"
+        );
+    }
+
+    public static SignInRequest createSignInRequestPasswordShorterThan1Characters() {
+
+        return new SignInRequest(
+                "user1@domain.com",
+                ""
+        );
+    }
+
+    public static SignInRequest createSignInRequestPasswordLongerThan20Characters() {
+
+        return new SignInRequest(
+                "user1@domain.com",
+                "a".repeat(21)
+        );
+    }
+
 }
--- src/test/java/com/chaplygin/task_manager/user/dto/SignInRequestTest.java
+++ src/test/java/com/chaplygin/task_manager/user/dto/SignInRequestTest.java
@@ -1,5 +1,72 @@
 package com.chaplygin.task_manager.user.dto;
 
+import com.chaplygin.task_manager.testDataFactory.SignInRequestFactory;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SignInRequestTest {
+
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @Test
+    public void givenNullEmailSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestNullEmail()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenNullPasswordSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestNullPassword()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
+
+
+    @Test
+    public void givenEmailShorterThan6CharactersSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestEmailShorterThan6Characters()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenEmailLongerThan50CharactersSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestEmailLongerThan50Characters()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenPasswordShorterThan1CharactersSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestPasswordShorterThan1Characters()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenEmailLongerThan20CharactersSignInRequest_whenValidate_thenNotValid() {
+        Set<ConstraintViolation<SignInRequest>> constraintViolations = validator.validate(
+                SignInRequestFactory.createSignInRequestPasswordLongerThan20Characters()
+        );
+
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
 
 }
